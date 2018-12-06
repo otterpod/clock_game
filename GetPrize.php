2ad28c94322684534a3f8729517c264eab6cda25
abellja <43049904+abellja@users.noreply.github.com>
2018-12-05 19:00:10 -0600

    Update GetPrize.php

--- GetPrize.php
+++ GetPrize.php
@@ -1,26 +1,32 @@
 <html>
 <?php
-		$user = 'csc390_fa18_abel';
-        $pass = 'T9PrGWAjpTD4Jqwy';	
+                $user = 'csc390_fa18_abel';
+        $pass = 'T9PrGWAjpTD4Jqwy';
         $dbh = new PDO('mysql:host=localhost;dbname=csc390_fa18_abel', $user, $pass);
-	//$checkemail = $_POST['email'];
-		$sql = 'SELECT * FROM prize order by RAND() limit 1 '; //pull this from database
-		$query = $dbh->prepare($sql);  //prepare
-		$query->execute();			   //execute
+        //$checkemail = $_POST['email'];
+                $sql = 'SELECT * FROM prize order by RAND() limit 1 '; //pull this from database
+                $query = $dbh->prepare($sql);  //prepare
+                $query->execute();                         //execute
 
-		// Get the results! This will return one row. If no row was found for the ID they gave we will get false.
-		$row = $query->fetch(PDO::FETCH_ASSOC);
-		
-		// Getting values out of the data structure returned by the database.
-		$name = $row['title'];
-		$description = $row['description'];
-		$min_price = $row['min_price'];
-		$max_price = $row['max_price'];
-		$url = $row['image_url'];
-		echo $name;
-		
-		
-		
+                // Get the results! This will return one row. If no row was found for the ID they gav$
+                $row = $query->fetch(PDO::FETCH_ASSOC);
+
+                // Getting values out of the data structure returned by the database.
+                $name = $row['title'];
+                $description = $row['description'];
+                $min_price = $row['min_price'];
+                $max_price = $row['max_price'];
+                $url = $row['image_url'];
+                echo $name;
+                echo '';
+        echo $description;
+        echo '';
+        echo $$min_price;
+        echo '';
+        echo $max_price;
+
+
 ?>
-		
+
 </html>
+
