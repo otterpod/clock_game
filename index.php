87ccf9dba89dcebb885225a4f3a0d9a58f50ffa1
abellja <43049904+abellja@users.noreply.github.com>
2018-12-05 19:57:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,25 @@
 	</header>
 	
 	<body>
+		<?php
+			$user = 'csc390_fa18_abel';
+			$pass = 'T9PrGWAjpTD4Jqwy';	
+			$dbh = new PDO('mysql:host=localhost;dbname=csc390_fa18_abel', $user, $pass);
+			$sql = 'SELECT * FROM prize order by RAND() limit 1 '; //pull this from database
+			$query = $dbh->prepare($sql);  //prepare
+			$query->execute();			   //execute
+			// Get the results! This will return one row. If no row was found for the ID they gave we will get false.
+			$row = $query->fetch(PDO::FETCH_ASSOC);
+
+			// Getting values out of the data structure returned by the database.
+			$name = $row['title'];
+			$description = $row['description'];
+			$min_price = $row['min_price'];
+			$max_price = $row['max_price'];
+			$url = $row['image_url'];
+			$RandPrice=rand($min_price,$max_price);
+		?>
+		<img><?php"$url"?></img>
 		<div class = "heading">
 			<h1>The Clock Game</h1>
 		</div>
